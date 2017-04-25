a98be6d426daf0f6694d32cbed7bc2e8f97918e9
Özgür <ozgur.gene@gmail.com>
2017-04-25 14:19:36 +0200

    Fix sorting

--- src/buildMessages.js
+++ src/buildMessages.js
@@ -28,7 +28,7 @@ export default function buildMessages(cache, buildStats) {
   const output = {};
   Object.keys(dict).forEach(locale => {
     output[locale] = output[locale] || [];
-    Object.keys(dict[locale]).sort((a, b) => a.localeCompare(b)).forEach(key => {
+    Object.keys(dict[locale]).sort((a, b) => a.toLocaleLowerCase().localeCompare(b.toLocaleLowerCase())).forEach(key => {
       const value = dict[locale][key];
       const escapedKey = key.replace(/"/g, '\\"');
       const escapedValue = value.replace(/"/g, '\\"');
